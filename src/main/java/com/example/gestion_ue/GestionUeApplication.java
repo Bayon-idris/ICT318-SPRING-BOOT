34263ff6f7ef938046eda3c767ebe61192ee4718
Idris_d237 <96464111+geekers-donald237@users.noreply.github.com>
2024-06-26 18:58:49 +0100

    setup project and dependency for jpa , thymleaf , jpa , spring security

---
+++ src/main/java/com/example/gestion_ue/GestionUeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestion_ue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionUeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionUeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestion_ue/GestionUeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestion_ue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionUeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
